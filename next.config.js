121e8c7f7777f89f3ffcb8056a0e80a7d0ec473e
kazuhideoki <cedar123pc@gmail.com>
2022-02-09 07:20:52 +0900

    localで起動成功

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// };
 
-module.exports = nextConfig
+// module.exports = nextConfig;
+
+module.exports = (phase, { defaultConfig }) => {
+  /**
+   * @type {import('next').NextConfig}
+   */
+  const nextConfig = {
+    reactStrictMode: true,
+  };
+  return nextConfig;
+};
