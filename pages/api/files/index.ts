f1574c0418c1de47832ceb40f20273bc1feef612
kazuhideoki <cedar123pc@gmail.com>
2022-02-10 06:59:09 +0900

    authでgoogle drive api接続

--- pages/api/files/index.ts
+++ pages/api/files/index.ts
@@ -1,5 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import { testGoogleApi } from "../../../server/google-drive.service";
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
+  testGoogleApi();
   res.status(200).json({ name: "John Doe" });
 };
--- server/google-drive.service.ts
+++ server/google-drive.service.ts
@@ -10,12 +10,14 @@ const SCOPES = ["https://www.googleapis.com/auth/drive.metadata.readonly"];
 // time.
 const TOKEN_PATH = "token.json";
 
-// Load client secrets from a local file.
-fs.readFile("credentials.json", (err, content) => {
-  if (err) return console.log("Error loading client secret file:", err);
-  // Authorize a client with credentials, then call the Google Drive API.
-  authorize(JSON.parse(content as any), listFiles);
-});
+export function testGoogleApi() {
+  // Load client secrets from a local file.
+  fs.readFile("credentials.json", (err, content) => {
+    if (err) return console.log("Error loading client secret file:", err);
+    // Authorize a client with credentials, then call the Google Drive API.
+    authorize(JSON.parse(content as any), listFiles);
+  });
+}
 
 /**
  * Create an OAuth2 client with the given credentials, and then execute the
