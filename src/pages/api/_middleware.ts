3278b3a081024d7ca4610a5153504c18126a9cf8
kazuhideoki <cedar123pc@gmail.com>
2022-02-24 08:14:55 +0900

    修正

--- src/pages/api/_middleware.ts
+++ src/pages/api/_middleware.ts
@@ -1,7 +1,7 @@
 import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
 
 // 色々活用法ある？
-export function middleware(req: NextRequest, ev: NextFetchEvent) {
+export default function middleware(req: NextRequest, ev: NextFetchEvent) {
   const res = NextResponse.next();
   return res;
 }
