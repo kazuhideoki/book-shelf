b0c81dae710ca733a44b48e1597fad19e2743548
kazuhideoki <cedar123pc@gmail.com>
2022-03-01 17:43:19 +0900

    authSignedInの追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,7 @@ import { AppProps } from "next/app";
 import { RecoilRoot, useRecoilValue } from "recoil";
 import { GlobalOverlay } from "../components/GloabalOverlay";
 import { SignIn } from "../components/Signin";
-import { authState } from "../recoil/atom/auth";
-
-interface P {}
+import { authSignedIn } from "../recoil/selector/auth";
 
 export default function App(props: AppProps) {
   return (
@@ -14,12 +12,12 @@ export default function App(props: AppProps) {
   );
 }
 
-function _App({ Component, pageProps }: AppProps<P>) {
-  const auth = useRecoilValue(authState);
+function _App({ Component, pageProps }: AppProps) {
+  const signedIn = useRecoilValue(authSignedIn);
 
   return (
     <>
-      {auth.initialized ? <Component {...pageProps} /> : <SignIn />}
+      {signedIn ? <Component {...pageProps} /> : <SignIn />}
       <GlobalOverlay />
     </>
   );
