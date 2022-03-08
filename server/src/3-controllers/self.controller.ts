15428e549c6a7b2e0ef5a65737e13298e4279f04
kazuhideoki <cedar123pc@gmail.com>
2022-03-08 17:54:05 +0900

    eslint修正

--- server/src/3-controllers/self.controller.ts
+++ server/src/3-controllers/self.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get } from '@nestjs/common';
 import { AuthContext } from '../../../front/src/old-server/helper/auth-context';
 import { ServerAuth } from '../../../type/model/auth';
 
-@Controller('')
+@Controller('self')
 export class SelfController {
   @Get()
   self(): ServerAuth {
