1d947ee64359fade6684646de64272a1e251af6a
kazuhideoki <cedar123pc@gmail.com>
2022-03-08 15:17:52 +0900

    "server/src/main"

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,11 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-  // const port = Number(process.env.PORT) || 3000;
+  // await app.listen(3000);
+  const port = Number(process.env.PORT) || 3000;
 
-  // console.log(`listen port: ${port}`);
+  console.log(`listen port: ${port}`);
 
-  // await app.listen(port);
+  await app.listen(port);
 }
 bootstrap();
