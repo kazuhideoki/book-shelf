3fd89e70254e0ca2fef3b2d344bd1be3efbfb17b
kazuhideoki <cedar123pc@gmail.com>
2022-03-09 22:43:25 +0900

    await app.listen(port, '0.0.0.0'); で

--- server/src/main.ts
+++ server/src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
 
   const port = Number(process.env.PORT) || 8080;
 
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
   console.log(`listen port: ${port}`);
 }
 bootstrap();
